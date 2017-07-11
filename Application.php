c61097d5cd7f10ed2787f942f3c8c7e4d4219a55
xuchao <85825770@qq.com>
2017-07-11 14:31:36 +0800

    update

--- Application.php
+++ Application.php
@@ -8,13 +8,9 @@
 namespace xutl\wechat;
 
 use Yii;
-use yii\base\InvalidRouteException;
-use yii\helpers\Url;
 use yii\web\ErrorHandler;
-use yii\web\NotFoundHttpException;
 use yii\web\Response;
 use yii\web\Session;
-use yii\web\UrlNormalizerRedirectException;
 use yii\web\User;
 
 /**
@@ -23,120 +19,6 @@ use yii\web\User;
  */
 class Application extends \yii\web\Application
 {
-    /**
-     * @var string the default route of this application. Defaults to 'site'.
-     */
-    public $defaultRoute = 'site';
-
-    /**
-     * @var array the configuration specifying a controller action which should handle
-     * all user requests. This is mainly used when the application is in maintenance mode
-     * and needs to handle all incoming requests via a single action.
-     * The configuration is an array whose first element specifies the route of the action.
-     * The rest of the array elements (key-value pairs) specify the parameters to be bound
-     * to the action. For example,
-     *
-     * ```php
-     * [
-     *     'offline/notice',
-     *     'param1' => 'value1',
-     *     'param2' => 'value2',
-     * ]
-     * ```
-     *
-     * Defaults to null, meaning catch-all is not used.
-     */
-    public $catchAll;
-
-    /**
-     * @var Controller the currently active controller instance
-     */
-    public $controller;
-
-    /**
-     * @inheritdoc
-     */
-    protected function bootstrap()
-    {
-        $request = $this->getRequest();
-        Yii::setAlias('@webroot', dirname($request->getScriptFile()));
-        Yii::setAlias('@web', $request->getBaseUrl());
-
-        parent::bootstrap();
-    }
-
-    /**
-     * Handles the specified request.
-     * @param Request $request the request to be handled
-     * @return Response the resulting response
-     * @throws NotFoundHttpException if the requested route is invalid
-     */
-    public function handleRequest($request)
-    {
-        if (empty($this->catchAll)) {
-            try {
-                list($route, $params) = $request->resolve();
-            } catch (UrlNormalizerRedirectException $e) {
-                $url = $e->url;
-                if (is_array($url)) {
-                    if (isset($url[0])) {
-                        // ensure the route is absolute
-                        $url[0] = '/' . ltrim($url[0], '/');
-                    }
-                    $url += $request->getQueryParams();
-                }
-                return $this->getResponse()->redirect(Url::to($url, $e->scheme), $e->statusCode);
-            }
-        } else {
-            $route = $this->catchAll[0];
-            $params = $this->catchAll;
-            unset($params[0]);
-        }
-        try {
-            Yii::trace("Route requested: '$route'", __METHOD__);
-            $this->requestedRoute = $route;
-            $result = $this->runAction($route, $params);
-            if ($result instanceof Response) {
-                return $result;
-            }
-
-            $response = $this->getResponse();
-            if ($result !== null) {
-                $response->data = $result;
-            }
-
-            return $response;
-        } catch (InvalidRouteException $e) {
-            throw new NotFoundHttpException(Yii::t('yii', 'Page not found.'), $e->getCode(), $e);
-        }
-    }
-
-    private $_homeUrl;
-
-    /**
-     * @return string the homepage URL
-     */
-    public function getHomeUrl()
-    {
-        if ($this->_homeUrl === null) {
-            if ($this->getUrlManager()->showScriptName) {
-                return $this->getRequest()->getScriptUrl();
-            }
-
-            return $this->getRequest()->getBaseUrl() . '/';
-        }
-
-        return $this->_homeUrl;
-    }
-
-    /**
-     * @param string $value the homepage URL
-     */
-    public function setHomeUrl($value)
-    {
-        $this->_homeUrl = $value;
-    }
-
     /**
      * Returns the error handler component.
      * @return ErrorHandler the error handler application component.
@@ -188,6 +70,7 @@ class Application extends \yii\web\Application
     public function coreComponents()
     {
         return array_merge(parent::coreComponents(), [
+            'view' => ['class' => 'xutl\wechat\View'],
             'request' => ['class' => 'xutl\wechat\Request'],
             'response' => ['class' => 'yii\web\Response'],
             'session' => ['class' => 'yii\web\Session'],
